80e9c2ecc704ef7f455d256ab1183265cadbb48c
2000jester <2000jester@gmail.com>
2017-05-02 11:23:53 +1000

    melting a friends brain

--- enemy.js
+++ enemy.js
@@ -1,8 +1,9 @@
-var enemy = function(group,x,y,row,column,stage){
+var enemy = function(group,x,y,row,column,stage,test){
   this.group = group
   this.x = x
   this.y = y
   this.row = row
   this.column = column
   this.stage = stage
+  this.test = test
 }
